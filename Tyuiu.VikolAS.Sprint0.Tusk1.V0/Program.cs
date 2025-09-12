e157dc51ae64c41fa9ea8273184ecfdc40c4f00f
alex vikol <sashavikol7@gmail.com>
2025-09-12 16:06:22 +0500

    Task1

---
+++ Tyuiu.VikolAS.Sprint0.Tusk1.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.VikolAS.Sprint0.Tusk1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello,World");
+            Console.ReadKey();
+            string name = "Александр";
+            int age = 18;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + "лет!");
+            Console.ReadKey();
+        }
+    }
+}
